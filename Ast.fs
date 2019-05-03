d9a88cd9b8bcb4f7cd6f01ec48929de444500769
steinuil <steenuil.owl@gmail.com>
2019-05-03 17:33:09 +0200

    first commit

---
+++ Ast.fs
@@ -0,0 +1,21 @@
+module Ast
+open System.Linq.Expressions
+
+
+
+type Statement =
+  | Let of string * Statement list option
+  | While of Statement list * Statement list
+  | Expr of Expression
+
+and Expression =
+  | IfElse of Statement list * Statement list * Statement list option
+  | FunCall of string * Statement list list
+  | Ident of string
+  | String of string
+  | Number of string
+  | Array of Statement list list
+
+
+type Decl =
+  | Proc of string * string list * Statement list
---
+++ Program.fs
@@ -0,0 +1,31 @@
+﻿open System
+open System.IO
+
+let lexStuff text =
+    let lexbuf = FSharp.Text.Lexing.LexBuffer<char>.FromTextReader text
+    let rec lexAll acc =
+        try
+            let x = Lexer.lex lexbuf
+            lexAll (x :: acc)
+        with _ ->
+            acc
+
+
+    lexAll [] |> List.rev
+
+
+let parse text =
+    let lexbuf = FSharp.Text.Lexing.LexBuffer<char>.FromTextReader text
+    let program = Parser.initial Lexer.lex lexbuf
+    program
+
+[<EntryPoint>]
+let main = function
+    | [|fname|] ->
+        use textReader = new StreamReader(fname)
+        lexStuff textReader |> printfn "%A"
+        use textReader = new StreamReader(fname)
+        parse textReader |> printfn "%A"
+        0
+    | _ ->
+        failwith "no inputs given"
