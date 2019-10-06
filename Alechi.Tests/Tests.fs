01c3f66f176d6cf93c9fe8f9f15507734c5b5c04
steinuil <steenuil.owl@gmail.com>
2019-10-07 00:32:56 +0200

    added tests proj

---
+++ Alechi.Tests/Tests.fs
@@ -0,0 +1,11 @@
+namespace Alechi.Tests
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type TestClass () =
+
+    [<TestMethod>]
+    member this.TestMethodPassing () =
+        Assert.IsTrue(true);
